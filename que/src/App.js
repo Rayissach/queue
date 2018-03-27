65503cf64bcdb52a9a3f82c042d1ed906d2b339e
Raymond Robinson <rayissach@Raymonds-MacBook-Pro.local>
2018-03-27 15:07:26 -0700

    adding signup form

--- que/src/App.js
+++ que/src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 class App extends Component {
+  
+  state = {
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+    email: "",
+
+  };
+
+  // handle any changes to the input fields
+  handleInputChange = event => {
+    // Pull the name and value properties off of the event.target (the element which triggered the event)
+    const { name, value } = event.target;
+
+    // Set the state for the appropriate input field
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, prevent the default event and alert the username and password
+  handleFormSubmit = event => {
+    event.preventDefault();
+    alert(`Username: ${this.state.username}\nPassword: ${this.state.password}`);
+    this.setState({ username: "", password: "" });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      <Login />
+    )
+}
 }
 
 export default App;
