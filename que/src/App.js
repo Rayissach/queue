8606ca7074f5c6c867a4a48895bf5fb5681726c6
Raymond Robinson <rayissach@raymonds-mbp.attlocal.net>
2018-03-27 17:29:06 -0700

    integrating particles

--- que/src/App.js
+++ que/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+// import Particles from 'react-particles-js';
+
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Login from './components/Login';
 
 class App extends Component {
@@ -28,9 +30,10 @@ class App extends Component {
   // When the form is submitted, prevent the default event and alert the username and password
   handleFormSubmit = event => {
     event.preventDefault();
-    alert(`Username: ${this.state.username}\nPassword: ${this.state.password}`);
-    this.setState({ username: "", password: "" });
+    alert(`First Name: ${this.state.firstname}\nLast Name: ${this.state.lastname}\nEmail: ${this.state.email}\nUsername: ${this.state.username}\nPassword: ${this.state.password}\n`);
+    this.setState({ firstname: "", lastname: "", email: "", username: "", password: ""});
   };
+
   render() {
     return (
       <Login />
--- que/src/components/Login.js
+++ que/src/components/Login.js
@@ -1,68 +1,39 @@
 import React from "react";
 // import React, { Component } from "react";
-// import './Login.css'
+import './Login.css'
 
-// class Login extends Component {
-//       // Setting the initial values of this.state.username and this.state.password
-//   state = {
-//     firstname: "",
-//     lastname: "",
-//     username: "",
-//     password: "",
-//     email: "",
-
-//   };
-
-//   // handle any changes to the input fields
-//   handleInputChange = event => {
-//     // Pull the name and value properties off of the event.target (the element which triggered the event)
-//     const { name, value } = event.target;
-
-//     // Set the state for the appropriate input field
-//     this.setState({
-//       [name]: value
-//     });
-//   };
-
-//   // When the form is submitted, prevent the default event and alert the username and password
-//   handleFormSubmit = event => {
-//     event.preventDefault();
-//     alert(`Username: ${this.state.username}\nPassword: ${this.state.password}`);
-//     this.setState({ username: "", password: "" });
-//   };
-
-//   render() {
-//     return (
     const Login = props => (
-        <div class="container">
-        <div class="signup">
-            <div class="row vertical-center">
-                <div class="signup-form col-md-12">
-                    <form class="form-signin" id="sign-up">
+        <div className="container">
+        <div className="signup">
+            <div className="row vertical-center">
+                <div className="signup-form col-md-12">
+                    <form className="form-signin" id="sign-up">
                         {/* <a href="index.html">
                             <i class="fas fa-arrow-left" style="margin: 20px 0 0 10px;"></i>
                         </a> */}
                         <img src="assets/images/logo.svg" alt="Logo"/>
                         <p>Welcome! Please create a new account.</p>
-                        <input type="text" class="form-control signup-username" name="username" placeholder="Username" required="" value={props.username} />
-                        <input type="text" class="form-control signup-email" name="email" placeholder="Email Address" required="" value={props.email} />
-                        <input type="text" class="form-control signup-first" name="first" placeholder="First name" required="" value={props.firstname} />
-                        <input type="text" class="form-control signup-last" name="last" placeholder="Last name" required="" value={props.lastname} />
+                        <select className="form-control signup-attendant" name="attendant" required="">
+                            <option value="DJ"> DJ
+                            </option>
+                            <option value="Party Animal"> Party Animal
+                            </option>
+                        </select>
+                        <input type="text" className="form-control signup-username" name="username" placeholder="Username" required="" value={props.username} />
+                        <input type="text" className="form-control signup-email" name="email" placeholder="Email Address" required="" value={props.email} />
+                        <input type="text" className="form-control signup-first" name="first" placeholder="First name" required="" value={props.firstname} />
+                        <input type="text" className="form-control signup-last" name="last" placeholder="Last name" required="" value={props.lastname} />
                         {/* <input type="" className="" */}
-                        <input type="password" class="form-control signup-password" name="password" placeholder="Password (Minimum 6 characters)"
+                        <input type="password" className="form-control signup-password" name="password" placeholder="Password (Minimum 6 characters)"
                             required="" value={props.password} />
-                        <button class="btn btn-lg btn-primary btn-block create-btn" type="submit" onClick={this.handleFormSubmit}>Create</button>
+
+                        <button className="btn btn-lg btn-primary btn-block create-btn" type="submit" onClick={this.handleFormSubmit}>Create</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
     )
-    
-
-//     );
-// }
-// }
 
 
 export default Login;
