bb664c0dcf225a1727bdd94934d7e47db885fd63
Raymond Robinson <rayissach@raymonds-mbp.attlocal.net>
2018-03-28 21:33:17 -0700

    signup form and particles.js are on the same page. Needs more work

--- que/src/App.js
+++ que/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-
 // import logo from './logo.svg';
-// import './App.css';
+import './index.css';
 import Login from './components/Login';
-import Wrapper from './components/Wrapper';
+// import Wrapper from './components/Wrapper';
+import ParticlesConfig from './particlesjs-config.json';
 
 class App extends Component {
   
@@ -37,123 +37,13 @@ class App extends Component {
 
   render() {
     return (
-      <Particles
-      params={{
-        "particles": {
-          "number": {
-            "value": 218,
-            "density": {
-              "enable": true,
-              "value_area": 394.57382081613633
-            }
-          },
-          "color": {
-            "value": "#ff00e0"
-          },
-          "shape": {
-            "type": "star",
-            "stroke": {
-              "width": 0,
-              "color": "#000000"
-            },
-            "polygon": {
-              "nb_sides": 5
-            },
-            "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-              "enable": false,
-              "speed": 1,
-              "opacity_min": 0.1,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 3,
-            "random": true,
-            "anim": {
-              "enable": false,
-              "speed": 38.36163836163836,
-              "size_min": 1.5984015984015985,
-              "sync": false
-            }
-          },
-          "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 6,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-            }
-          }
-        },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "bubble"
-            },
-            "onclick": {
-              "enable": true,
-              "mode": "bubble"
-            },
-            "resize": true
-          },
-          "modes": {
-            "grab": {
-              "distance": 400,
-              "line_linked": {
-                "opacity": 1
-              }
-            },
-            "bubble": {
-              "distance": 383.61638361638364,
-              "size": 10,
-              "duration": 2,
-              "opacity": 0.17582417582417584,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 71.92807192807193,
-              "duration": 0.4
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
-            }
-          }
-        },
-        "retina_detect": true
-      
-      }}>
-      <Wrapper>
-       <Login />
-       </Wrapper>
-       </Particles>
-      
+      <div>
+      <Particles params={ParticlesConfig} id="particles-js" className="particles-style" width="100%" height="100%">
+      </Particles>
+       {/* <Wrapper> */}
+       <Login className="login-style"/>
+       {/* </Wrapper> */}
+      </div>
     )
 }
 }
